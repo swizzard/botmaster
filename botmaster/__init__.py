7b44139217e6653cd30f72058fe6849671ba2605
Sam Raker <sam.raker@gmail.com>
2015-06-16 16:43:26 -0400

    import-related bugfix

--- botmaster/__init__.py
+++ botmaster/__init__.py
@@ -1,5 +1,5 @@
 # coding=utf8
 __author__ = 'Sam Raker'
 
-from decorators import tweet, gen_tweet
-from auth import _auth, env_auth
+from .decorators import tweet, gen_tweet
+from .auth import _auth, env_auth
