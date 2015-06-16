b8feb0e37ad43a9e83741211281e0a725f32619d
Sam Raker <sam.raker@gmail.com>
2015-06-15 20:52:04 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="botmaster",
-      version="0.2",
+      version="0.2.1",
       description="Twitter Bot Manager",
       author="Sam Raker",
       author_email="sam.raker@gmail.com",
